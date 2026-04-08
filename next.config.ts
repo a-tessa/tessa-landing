64294c719a260c31eb176f722701a6f9f664d0c7
Luis Fritzen <luis.fritzen@innyx.com>
2026-04-08 01:32:06 -0300

    Add next-video integration; update .gitignore to manage video files; enhance package.json and pnpm-lock.yaml for new dependencies; create video.d.ts for type definitions; add initial video metadata JSON file.

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+import { withNextVideo } from "next-video/process";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -29,4 +30,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withNextVideo(nextConfig);
